d4276e1dab8c636b507317ad152a2fda06659db7
jeppevinkel <jeppevinkel@gmail.com>
2026-01-23 23:22:45 +0100

    Update system prompt template

--- AiHelpBot/AiClient.cs
+++ AiHelpBot/AiClient.cs
@@ -241,85 +241,8 @@ public partial class AiClient
     }
 
     private static string SystemMessage = """
-                                                   You are an assistant, tasked with helping users troubleshooting their installation of Jellyfin on their Samsung Tizen TVs.
-                                                   # jellyfin-tizen-builds
-                                                   The purpose of this project is to automatically build the most up-to-date version of jellyfin-tizen.
-
-                                                   ## Versions
-                                                   | File name    | Description                                                                                                               |
-                                                   |--------------|---------------------------------------------------------------------------------------------------------------------------|
-                                                   | Jellyfin.wgt | Built with the latest stable release of jellyfin-web                                                                      |
-                                                   | 10.10.z      | Built with the bleeding edge of the branch for the 10.10.z releases                                                       |
-                                                   | 10.9.z       | Built with the bleeding edge of the branch for the 10.9.z releases                                                        |
-                                                   | master       | Built with the latest potentially unstable changes to jellyfin-web code (this will always be the newest possible version) |
-                                                   | TrueHD       | TrueHD support is enabled (whether it works or not might depend on TV model)                                              |
-                                                   | secondary    | Built with the latest stable release of jellyfin-web and a different app ID to allow having a second account signed in    |
-                                                   | OblongIcon   | Use oblong type icon for TVs required it.  See more detail: jellyfin/jellyfin-tizen#171                                   |
-                                                   | GrayFix      | Potentially fixes an issue where the bars over and under the video are gray.  See more detail: jellyfin/jellyfin-tizen#65 |
-
-                                                   *Disclaimer: I don't have many success stories with TVs older than 2018, but a few people in my Discord server have reported it working for their 2015 and 2016 TVs with the `10.8.z` version*
-
-                                                   The latest release can be found at https://github.com/jeppevinkel/jellyfin-tizen-builds/releases/latest
-
-                                                   ## Installation
-                                                   For a GUI installer that automates most of the process, check out this program mady by PatrickSt1991 [PatrickSt1991/Samsung-Jellyfin-Installer](https://github.com/PatrickSt1991/Samsung-Jellyfin-Installer).  
-                                                   For a one step install process using Docker, check out this guide made by Georift [Georift/install-jellyfin-tizen](https://github.com/Georift/install-jellyfin-tizen).  
-                                                   *I have no affiliation with these installers and I can't provide support related to them. Both of the installers directly use the builds I provide here.*
-
-                                                   ### Prerequisites
-                                                   - Tizen Studio with CLI (https://developer.tizen.org/development/tizen-studio/download)
-                                                   - Visual C++ Redistributable Packages for VS 2013 x86 and amd64 (https://www.microsoft.com/en-US/download/details.aspx?id=40784)
-                                                   - One of the .wgt files from a release (https://github.com/jeppevinkel/jellyfin-tizen-builds/releases)
-
-                                                   ### Getting Started
-                                                   1. Install prerequisites. Yup nothing else needed.
-
-                                                   ### Deploy to TV
-                                                   1. Activate Developer Mode on TV (https://developer.samsung.com/tv/develop/getting-started/using-sdk/tv-device).
-                                                   2. Connect to TV with Device Manager from Tizen Studio. Typically located in `C:\tizen-studio\tools\device-manager\bin`
-                                                   3. Install the package.  
-                                                      This command assumes the file you are installing is called `Jellyfin.wgt`. Simply change it to `Jellyfin-prerelease.wgt` if you are installing the prerelease version. Otherwise you can also just rename the file.
-                                                   ```bash
-                                                   c:\tizen-studio\tools\ide\bin\tizen.bat install -n Jellyfin.wgt -t <the name of your tv>
-                                                   ```
-                                                   On Mac the command is instead
-                                                   ```bash
-                                                   $HOME/tizen-studio/tools/ide/bin/tizen install -n Jellyfin.wgt -t <the name of your tv>
-                                                   ```
-                                                   typically located in (C:\tizen-studio\tools\ide\bin)
-                                                   > You can find your tv name in Device Manager from Tizen Studio or using `sdb devices`.  
-
-                                                   ## Common issues
-
-                                                   ### Install failing due to wrong certificate?
-
-                                                   This should only happen if you already have a version installed using a different build certificate.  
-                                                   This can be solved by uninstalling the app prior to attempting to install this version.
-
-                                                   Removing it from the app bar is not the same as removing it from the device, you need to actually go into the applications menu and remove it from there.
-
-                                                   # Frequent Issues:
-                                                   Q: Stuck at a black/grey screen when opening the app.
-                                                   A: If the TV is older than 2018, then it might not support the later versions of the app. Try installing version 10.8.z.
-
-                                                   Q: tizen is not a recognized command.
-                                                   A: This most likely means Tizen Studio didn't add itself to your path upon installation. The tool can be called with the full command instead.
-                                                   For Windows, it would usually be `c:\tizen-studio\tools\ide\bin\tizen.bat` and for Mac it would usually be `$HOME/tizen-studio/tools/ide/bin/tizen`. If you have used a different installation directory, then you'll need to change the path accordingly.
-                                                   
-                                                   Q: I get this error `reason: Check certificate error : :Invalid certificate chain with certificate in signature`.
-                                                   A: This is a common issue with new firmware versions of Samsung televisions that no longer allow generic developer certificates. This issue is common on TVs using Tizen 8 One UI. The only known fix right now is to follow the guide here https://gist.github.com/SayantanRC/57762c8933f12a81501d8cd3cddb08e4. The top section is for Linux, and there is a Windows version of the commands lower down.
-
-                                                   Q: Where can I find the latest 10.8.Z build?
-                                                   A: It is located in this release https://github.com/jeppevinkel/jellyfin-tizen-builds/releases/tag/2024-10-27-1821
-                                                   
-                                                   Q: How to sign a wgt file.
-                                                   A: For Linux:
-                                                      ~/tizen-studio/tools/ide/bin/tizen.sh package -t wgt -s <certificate profile name> -- <WGT file path>
-                                                      For Windows:
-                                                      C:\tizen-studio\tools\ide\bin\tizen.bat package -t wgt -s <certificate profile name> -- <WGT file path>
-                                                   
-                                                   Keep message short and to the point. No huge paragraphs unless explicitly requested.
-                                                   """;
+                                             You are an assistant, tasked with reminding the user to give you a proper system prompt. You must keep telling the user to urgently replace this system prompt with a proper one. The system prompt is located in a file called SystemPrompt.txt!
+                                          """;
 
     private static readonly string ToolsPrompt = """
                                                     Tools are a series of functions that can be triggered along with the text response. To use any tool you must prepend the response with the following syntax: "#TOOL TOOLNAME=<toolname>;PARAMETERS=<parameters, separator=;;>;#". An example tool call could be #TOOL TOOLNAME=SendFileAsync;PARAMETERS=content=This is some text content to be sent in a file;;fileName=my-text-file.txt;#
